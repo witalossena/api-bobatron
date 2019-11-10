3969c6c74f72690781165787d2f6a09e37b1e396
witalo <sousawitalo.gmail.com>
2019-11-09 21:36:33 -0300

    metodo get adicionado

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
-// mongoose.connect('mongodb+srv://deploy:Ww81697895@cluster0-jmhfk.mongodb.net/test?retryWrites=true&w=majority',
-//  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://deploy:Ww81697895@cluster0-jmhfk.mongodb.net/test?retryWrites=true&w=majority',
+ { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
  
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://deploy:Ww81697895@cluster0-jmhfk.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useUnifiedTopology: true});
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://deploy:@cluster0-jmhfk.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
