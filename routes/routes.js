492cb7c35028dfe1ed31b159b1ce80b70f9871ad
Witalo Sousa Sena <witalo.sena@cmc.com.br>
2019-11-11 11:24:38 -0300

    rotas funcionario e estoque add

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const FuncController = require('../controllers/controler')
 const EstoqController = require('../controllers/controller.estoque')
 
 module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.status(200).send('rotas /estoque /funcionarios')
+    })
     app.get('/funcionarios', FuncController.getFunc)  
     app.get('/estoque', EstoqController.getEstoque )
     app.post('/novo-funcionario', FuncController.addFunc) 
