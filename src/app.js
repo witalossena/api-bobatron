3607f6e8b4dad531ba93c2a572d84579b26ee5c6
witalo <sousawitalo.gmail.com>
2019-11-11 16:22:16 -0300

    rotas criadas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const router = require('../routes/routes');
-// const db = require('../config/db');
+const router = require('./routes/routes');
+const db = require('./config/db');
 const bodyParser = require('body-parser')
 require('dotenv').config();
 const app = express()
@@ -12,15 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_HOST,
+// mongoose.connect(process.env.DB_HOST,
 
-   {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
+//    {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true
       
-  });
+//   });
 
 
 router(app)
